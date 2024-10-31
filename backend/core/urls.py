fc9976a83c1d181d46b15a72950a344347b31612
EnriqueVal09 <quiquechavezrm@gmail.com>
2024-10-30 20:12:57 -0600

    Adding the apps' urls to core.urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('apps.accounts.urls')),
+    path('notes/', include('apps.notes.urls')),
+    path('books/', include('apps.books.urls')),
 ]
